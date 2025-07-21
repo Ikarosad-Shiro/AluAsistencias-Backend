2c1b18ab1294fe55ea9932eb47bc84e8cae429f4
Ikarosad-Shiro <desconexionparcial@gmail.com>
2025-07-21 13:07:04 -0600

    Comunicacion de Front -> Back Restructuracion del index v.1.3.3

--- routes/pingRoutes.js
+++ routes/pingRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/ping', (req, res) => {
   console.log('📨 Bien y tu amorcito de mi vida?💘');
-  res.status(200).send('pong');
+   res.status(200).json({ mensaje: '🏓 Pong! Backend activo' });
 });
 
 module.exports = router;
